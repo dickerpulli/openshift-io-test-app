5d3608488789f39d87139c58c7621b2ecc9c145c
Thomas Bosch <tbosch@gmx.de>
2018-10-30 12:24:31 +0000

    Spring Boot 2.x

--- src/main/java/io/openshift/booster/Booster.java
+++ src/main/java/io/openshift/booster/Booster.java
@@ -1,4 +1,11 @@
-package io.openshift.booster;                                                      
-public class Booster {                                                      
- public static void main(String[] args) { }                                                      
-} 
+package io.openshift.booster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booster {
+    public static void main(String[] args) {
+        SpringApplication.run(Booster.class, args);
+    }
+}
